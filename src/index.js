76057d0d4e0502fb7abb79b6b8fc6fe8d811b4d2
Luke Ghenco <lukeghenco@gmail.com>
2017-02-13 22:16:19 -0500

    Update index.js solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 
-import Actors from './components/Actors'
-import App from './components/App'
-import Directors from './components/Directors'
-import Home from './components/Home'
-import Movies from './components/Movies'
+// routes
+import routes from './routes';
 
+// components
+import Actors from './components/Actors';
+import App from './components/App';
+import Directors from './components/Directors';
+import Home from './components/Home';
+import Movies from './components/Movies';
 
-render(
-    (<Router history={browserHistory} >
-      <Route path="/" component={App} />
-      <Route path="/movies" component={Movies} />
-      <Route path="/actors" component={Actors} />
-      <Route path="/directors" component={Directors} />
-    </Router>),
-    document.getElementById('container')
-)
+render (
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('root')
+);
